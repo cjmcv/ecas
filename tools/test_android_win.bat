1dd66e059d573e52bc5f71ee55a917a233663266
cjmcv <398342899@qq.com>
2022-11-19 15:16:20 +0000

    Add docs && Update cmakelist.

--- tools/test_android_win.bat
+++ tools/test_android_win.bat
@@ -1,4 +1,4 @@
-adb push ../build/demo.out /data/local/tmp/dlas/demo.out
-adb push ../build/libdlas.a /data/local/tmp/dlas/libdlas.a
+adb push ../bin/demo.out /data/local/tmp/dlas/demo.out
+adb push ../bin/libdlas.a /data/local/tmp/dlas/libdlas.a
 
 adb shell "chmod 777 -R ./data/local/tmp/dlas/ && ./data/local/tmp/dlas/demo.out"
